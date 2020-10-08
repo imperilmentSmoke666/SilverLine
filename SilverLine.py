22a96986a27f386e44fad033fb963e643e897577
imperilmentSmoke666 <72558619+imperilmentSmoke666@users.noreply.github.com>
2020-10-08 14:14:29 +0100

    Add files via upload

---
+++ SilverLine.py
@@ -0,0 +1,21 @@
+import requests
+from requests.exceptions import HTTPError
+
+with open("sites.txt") as fp:
+    Lines = fp.readlines() 
+
+for line in Lines:
+    try:
+        response = requests.get(line)
+
+        # If the response was successful, no Exception will be raised
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')  # Python 3.6
+    except Exception as err:
+        print(f'Other error occurred: {err}')  # Python 3.6
+    else:
+        print('Success! >' + response.url)
+        with open("working.txt", "a") as fp:
+            fp.writelines(line)
+            fp.close()
